7745ae7ab4c31fc993a23943c08ea07f680d02d7
Brandon Knox <bknox.dev@gmail.com>
2021-04-22 04:56:15 -0700

    Completed RatingView and added to AddBookView

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -20,7 +20,27 @@ struct RatingView: View {
     var onColor = Color.yellow
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1) { number in
+                self.image(for: number)
+                    .foregroundColor(number > self.rating ? self.offColor : self.onColor)
+                    .onTapGesture {
+                        self.rating = number
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
     }
 }
 
