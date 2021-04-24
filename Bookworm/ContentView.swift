d36b9300e9cc5dece706c10083f152048b538ef9
Brandon Knox <bknox.dev@gmail.com>
2021-04-23 22:54:48 -0700

    Added function deleteBooks to allow for removal from list.  Added paired .onDelete method, and created EditButton in navigationBarItems.  Cleaned up indentions that were off.

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -21,29 +21,40 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(books, id: \.self) { book in NavigationLink(destination: DetailView(book: book)) {
-                    EmojiRatingView(rating: book.rating)
-                        .font(.largeTitle)
-                    
-                    VStack(alignment: .leading) {
-                        Text(book.title ?? "Unknown title")
-                            .font(.headline)
-                        Text(book.author ?? "Unknown author")
-                            .foregroundColor(.secondary)
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown author")
+                                .foregroundColor(.secondary)
+                        }
                     }
-                }}
-            }
-                .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing: Button(action: {
-                    self.showingAddScreen.toggle()
-                    }) {
-                    Image(systemName: "plus")
-                    }
-                )
-                .sheet(isPresented: $showingAddScreen) {
-                    
-                    AddBookView().environment(\.managedObjectContext, self.moc)
                 }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                self.showingAddScreen.toggle()
+                }) {
+                Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddScreen) {
+                
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
