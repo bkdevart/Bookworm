d80a68ead4a3f6865fa90dcbc6c88c82a135ca54
Brandon Knox <bknox.dev@gmail.com>
2021-04-23 22:44:07 -0700

    Fleshed out DetailView to display image, rating, and other properties of Book.  Modified ContentView's NavigationLink to point to DetailView.

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(books, id: \.self) { book in NavigationLink(destination: Text(book.title ?? "Unknown title")) {
+                ForEach(books, id: \.self) { book in NavigationLink(destination: DetailView(book: book)) {
                     EmojiRatingView(rating: book.rating)
                         .font(.largeTitle)
                     
